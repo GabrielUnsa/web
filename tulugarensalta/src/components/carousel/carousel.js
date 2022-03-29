b69863b53157818c1c90731a9ac2b8735e5e8759
GabrielUnsa <gab24wal@gmail.com>
2022-03-29 14:22:26 -0300

    added

--- tulugarensalta/src/components/carousel/carousel.js
+++ tulugarensalta/src/components/carousel/carousel.js
@@ -1,43 +1,31 @@
 import { Carousel  } from 'react-bootstrap';
-
+import imgC1 from '../../assets/img/coursel/8A_1.jpg';
+import imgC2 from '../../assets/img/coursel/8A_2.jpg';
+import imgC3 from '../../assets/img/coursel/Alvarado2A_1.jpg';
+import imgC4 from '../../assets/img/coursel/Alvarado2A_2.jpg';
+import imgC5 from '../../assets/img/coursel/Alvarado3A_1.jpg';
+import imgC6 from '../../assets/img/coursel/Alvarado3A_2.jpg';
+import imgC7 from '../../assets/img/coursel/Gorriti5D_1.jpg';
+import imgC8 from '../../assets/img/coursel/Gorriti5D_2.jpg';
+import './carousel.css';
 
 const CarouselGallery = () => (
 <div>
-<Carousel variant="dark">
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=First slide&bg=f5f5f5"
-    alt="First slide"
-  />
-  <Carousel.Caption>
-    <h5>First slide label</h5>
-    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=Second slide&bg=eee"
-    alt="Second slide"
-  />
-  <Carousel.Caption>
-    <h5>Second slide label</h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-    alt="Third slide"
-  />
-  <Carousel.Caption>
-    <h5>Third slide label</h5>
-    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-</Carousel>
+  <Carousel>
+    <Carousel.Item>
+      <img
+        className="flex"
+        src={imgC2}
+        alt="First slide"
+        width='60%'
+        height='400'
+      />
+      <Carousel.Caption>
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
 </div>
 );
 export default CarouselGallery;
