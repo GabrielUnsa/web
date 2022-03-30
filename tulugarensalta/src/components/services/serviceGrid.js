2d4ab6cb79c1892963dd12a8ee794f845581d800
GabrielUnsa <gab24wal@gmail.com>
2022-03-30 19:04:16 -0300

    added

--- tulugarensalta/src/components/services/serviceGrid.js
+++ tulugarensalta/src/components/services/serviceGrid.js
@@ -1,18 +1,47 @@
 import { Container, Row, Col } from 'react-bootstrap'
-
+import { TiWiFi } from 'react-icons/ti';
+import { FaBath, FaRegSnowflake } from 'react-icons/fa';
+import { GiHomeGarage, GiElevator } from 'react-icons/gi';
+import { FiTv } from 'react-icons/fi';
+import { MdKitchen, MdOutlineLocalLaundryService, MdLocationCity } from 'react-icons/md';
 
 const ServiceGrid  = () => (
   <Container>
     <h1 class="display-4"> Servicios </h1>
-    <Row className="justify-content-md-center">
-      <Col md="auto">
-        
+    <br />
+    <Row className="justify-content-start">
+      <Col >
+        <h2> <TiWiFi /> WiFi </h2>
       </Col>
-      <Col md="auto">
-      
+      <Col>
+        <h2> <MdOutlineLocalLaundryService /> Lavarropas </h2>
       </Col>
-      <Col md="auto">
-
+      <Col>
+        <h2> <GiHomeGarage /> Cochera </h2>
+      </Col>
+    </Row>
+    <br />
+    <Row className="justify-content">
+      <Col>
+        <h2> <FiTv /> Tv </h2>
+      </Col>
+      <Col>
+        <h2> <MdKitchen /> Cocina </h2>
+      </Col>
+      <Col>
+        <h2> <GiElevator /> Acensor </h2>
+      </Col>
+    </Row>
+    <br />
+    <Row className="justify-content">
+      <Col>
+        <h2> <FaRegSnowflake /> Aire Acondicionado </h2>
+      </Col>
+      <Col>
+        <h2> <FaBath /> Baño Privado </h2>
+      </Col>
+      <Col>
+        <h2> <MdLocationCity /> Ubicacion Centrica </h2>
       </Col>
     </Row>
   </Container>
