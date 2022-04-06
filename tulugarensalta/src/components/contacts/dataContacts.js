0de93ec8ff82f08e997284259a0bbff8f7232614
GabrielUnsa <gab24wal@gmail.com>
2022-04-05 21:50:51 -0300

    added

--- tulugarensalta/src/components/contacts/dataContacts.js
+++ tulugarensalta/src/components/contacts/dataContacts.js
@@ -2,6 +2,7 @@ import { Container, Row } from 'react-bootstrap';
 import { BsWhatsapp, BsInstagram } from 'react-icons/bs';
 import { SiMaildotru } from 'react-icons/si';
 import { ImFacebook } from 'react-icons/im';
+import './contacts.css';
 
 export default function DataContact(){
   return(
@@ -9,19 +10,19 @@ export default function DataContact(){
       <h3> Contactanos </h3>
       <br />
       <Row md="auto" className="justify-content-start">
-        <a href="https://www.google.com.ar" className=""> <h3> <BsWhatsapp /> {' '} +54 3874049071 </h3></a>
+        <a href="https://api.whatsapp.com/send?phone=+543874049071" target="_blank" rel="noopener noreferrer" className="link-dark"> <h3> <BsWhatsapp /> {' '} +54 3874049071 </h3></a>
       </Row>
       <br />   
       <Row md="auto" className="justify-content-start">
-        <h3> <SiMaildotru /> {' '} tulugarensalta@gmail.com </h3>
+        <a href="mailto:tulugarensalta@gmail" className="link-dark" target="_blank" rel="noopener noreferrer"> <h3> <SiMaildotru /> {' '} tulugarensalta@gmail.com </h3> </a>
       </Row>
       <br />
       <Row md="auto" className="justify-content-start">
-        <h3> <ImFacebook /> {' '} Tu Lugar en Salta </h3>
+        <a href="https://www.facebook.com/RamiroGutierrezTLeS" className="link-dark" target="_blank" rel="noopener noreferrer"> <h3> <ImFacebook /> {' '} Tu Lugar en Salta </h3> </a>
       </Row>
       <br />
       <Row md="auto" className="justify-content-start">
-        <h3> <BsInstagram /> {' '} _tulugarensalta_ </h3>
+<a href="https://www.instagram.com/_tulugarensalta_/" className="link-dark" target="_blank" rel="noopener noreferrer"> <h3> <BsInstagram /> {' '} _tulugarensalta_ </h3> </a>
       </Row>
     </Container>
   );
