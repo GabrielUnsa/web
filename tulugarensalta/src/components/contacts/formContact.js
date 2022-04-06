92354fc8af20271749f2ed10c30b3668d8f9b24a
GabrielUnsa <gab24wal@gmail.com>
2022-04-06 17:10:49 -0300

    added

--- tulugarensalta/src/components/contacts/formContact.js
+++ tulugarensalta/src/components/contacts/formContact.js
@@ -1,8 +1,14 @@
 import { Row, Col, Form, FloatingLabel, Button } from 'react-bootstrap';
+import emailjs from 'eamiljs-com';
 
 export default function FormContact(){
+  function sendEmail(e){
+    e.preventDefault();
+    emailjs.sendForm('service_oqsdh7t','template_yzwzhx4',e.target,'KTC9fGDgw5scUYgrl').then();
+  }
+
   return(
-    <Form>
+    <Form onSubmit={sendEmail}>
       <h3> Envianos un Mensaje </h3>
       <br />
       <Row className="g-2">
