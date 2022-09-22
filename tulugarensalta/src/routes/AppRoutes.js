4e9059290ce5a9fcedc53e9fd58ec3607f609700
GabrielUnsa <gab24wal@gmail.com>
2022-09-22 18:09:02 -0300

    added

--- tulugarensalta/src/routes/AppRoutes.js
+++
@@ -1,12 +0,0 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './../pages/home/home';
-
-export default function AppRouters(){
-  return(
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-      </Routes>
-    </Router>
-  );
-}
